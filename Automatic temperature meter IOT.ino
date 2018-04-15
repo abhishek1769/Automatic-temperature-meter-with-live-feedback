d4fad6e10799cbe4e16cf392dbc33aee14139bd7
Abhishek Kumar <33525647+abhishek1769@users.noreply.github.com>
2018-04-15 16:38:16 +0530

    Add files via upload

---
+++ Automatic temperature meter IOT.ino
@@ -0,0 +1,60 @@
+#include<DHT.h>
+#include<ESP8266WiFi.h>
+#include<WiFiClient.h>
+#include<ThingSpeak.h>
+#define DHTPIN D5
+#define DHTTYPE DHT11
+DHT dht(DHTPIN,DHTTYPE);
+const char*ssid="freewifi";
+const char*password="freepassword";
+WiFiClient client;
+unsigned long myChannelNumber=292823;
+const char * myWriteAPIKey="R1WCCO1BIDCJ8ATQ";
+uint8_t temperature,humidity,k=0,l=0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+dht.begin();
+delay(10);
+Serial.println();
+Serial.println();
+Serial.print("Connecting to");
+Serial.println(ssid);
+WiFi.begin(ssid,password);
+while(WiFi.status() != WL_CONNECTED)
+{
+  delay(500);
+  Serial.print("[.");
+}
+Serial.println("");
+Serial.println("WiFi connected");
+Serial.println(WiFi.localIP());
+ThingSpeak.begin(client);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+static boolean data_state = false;
+temperature=dht.readTemperature();
+humidity=dht.readHumidity();
+Serial.print(temperature);
+Serial.println("C");
+Serial.print("Humidity value is:");
+Serial.print(humidity);
+Serial.println("%");
+if(temperature<255){
+  k=temperature;
+}
+if(humidity<255){
+  l=humidity;
+}
+if(data_state){
+  ThingSpeak.writeField(myChannelNumber, 1,k,myWriteAPIKey);
+data_state=false;
+}
+else{
+  ThingSpeak.writeField(myChannelNumber,2,l,myWriteAPIKey);
+  data_state=true;
+}
+delay(30000);
+}
---
+++ TEmperature_Display.ino
@@ -0,0 +1,54 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin(16,2);
+pinMode(6,INPUT);
+}
+float temp1=0;
+float humid1=0;
+float temp=0;
+float humid=0;
+int sw1=1;
+void loop() {
+  // put your main code here, to run repeatedly:
+temp=digitalRead(6);
+humid=digitalRead(6);
+temp1=temp;
+humid1=humid;
+if(sw1==1){
+if(temp1==temp){
+  lcd.setCursor(0,0);
+  lcd.print("TEMPERATURE=");
+  lcd.setCursor(13,0);
+  lcd.print(temp);
+  delay(2000);
+  sw1=2;
+}else{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("TEMPERATURE=");
+  lcd.setCursor(13,0);
+  lcd.print(temp1);
+  delay(2000);
+  sw1=2;
+}
+}else if(sw1==2){
+if(humid1==humid){
+  lcd.setCursor(0,1);
+  lcd.print("HUMIDITY=");
+  lcd.setCursor(10,1);
+  lcd.print(humid);
+  delay(2000);
+  sw1=1;
+}else{
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("HUMIDITY=");
+  lcd.setCursor(10,1);
+  lcd.print(humid1);
+  delay(2000);
+  sw1=1;
+}
+}
+}
